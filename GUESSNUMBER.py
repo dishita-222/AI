4d265d4b8d4e7cb95e44f19ab7624a6571aaf1eb
Dishita Sibal <74229025+dishita-222@users.noreply.github.com>
2023-03-18 11:42:39 +0530

    Add files via upload

---
+++ GUESSNUMBER.py
@@ -0,0 +1,26 @@
+import random
+
+# Generate a random number between 1 and 100
+secret_number = random.randint(1, 100)
+
+# Initialize the number of guesses to 0
+num_guesses = 0
+
+# Loop until the player guesses the number
+while True:
+    # Get the player's guess from them 
+    guess = int(input("Guess the number between 1 and 100: "))
+    
+    # Increment the number of guesses
+    num_guesses += 1
+    
+    # Check if the player guessed the correct number
+    if guess == secret_number:
+        print("Congratulations! You guessed the number in", num_guesses, "guesses.")
+        break
+    
+    # Give the player a hint if their guess was too high or too low
+    if guess < secret_number:
+        print("Too low. Guess again.")
+    else:
+        print("Too high. Guess again.")
